cfcfa9e6a529a212ca85400b4600dc381d4dd670
Евгений Карпов <evgeniy_karpov91@inbox.ru>
2021-01-27 18:26:04 +0300

    делает правки 2

--- build/js/script.js
+++ build/js/script.js
@@ -3,19 +3,33 @@
 const header = document.querySelector(".navigation");
 const navButton = document.querySelector(".navigation__button-menu");
 const menu = document.querySelector(".navigation__list");
-
+const navLink = document.querySelectorAll(".navigation__item");
 
 header.classList.remove("navigation--nojs");
 menu.classList.remove("navigation__list--nojs");
 
+const closeMenu = function () {
+  header.classList.remove("navigation--opened");
+  header.classList.add("navigation--close");
+  menu.classList.add("navigation__list--close");
+}
+
+const openMenu = function () {
+  header.classList.remove("navigation--close");
+  menu.classList.remove("navigation__list--close");
+  header.classList.add("navigation--opened");
+}
+
 navButton.addEventListener("click", function() {
   if (header.classList.contains("navigation--close")) {
-    header.classList.remove("navigation--close");
-    menu.classList.remove("navigation__list--close");
-    header.classList.add("navigation--opened");
+    openMenu();
   } else {
-    header.classList.remove("navigation--opened");
-    header.classList.add("navigation--close");
-    menu.classList.add("navigation__list--close");
+    closeMenu();
   }
 });
+
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener("click", function() {
+    closeMenu();
+  });
+}
