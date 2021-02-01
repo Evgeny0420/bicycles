2f8a7dfdee45f8068b3a7f3dd38dc0a591e87406
Евгений Карпов <evgeniy_karpov91@inbox.ru>
2021-02-01 20:52:22 +0300

    делает правки

--- source/js/main.js
+++ source/js/main.js
@@ -6,22 +6,22 @@
   var menu = document.querySelector('.navigation__list');
   var navLink = document.querySelectorAll('.navigation__item');
 
-  header.classList.remove('navigation--nojs');
-  menu.classList.remove('navigation__list--nojs');
+  if (header && navButton && menu && navLink) {
+    header.classList.remove('navigation--nojs');
+    menu.classList.remove('navigation__list--nojs');
 
-  var closeMenu = function () {
-    header.classList.remove('navigation--opened');
-    header.classList.add('navigation--close');
-    menu.classList.add('navigation__list--close');
-  };
+    var closeMenu = function () {
+      header.classList.remove('navigation--opened');
+      header.classList.add('navigation--close');
+      menu.classList.add('navigation__list--close');
+    };
 
-  var openMenu = function () {
-    header.classList.remove('navigation--close');
-    menu.classList.remove('navigation__list--close');
-    header.classList.add('navigation--opened');
-  };
+    var openMenu = function () {
+      header.classList.remove('navigation--close');
+      menu.classList.remove('navigation__list--close');
+      header.classList.add('navigation--opened');
+    };
 
-  if (header) {
     navButton.addEventListener('click', function () {
       if (header.classList.contains('navigation--close')) {
         openMenu();
