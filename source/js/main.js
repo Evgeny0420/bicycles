508ff52eb229af74805ae3e374f9f6bbe5f0dd17
Евгений Карпов <evgeniy_karpov91@inbox.ru>
2021-02-03 18:51:34 +0300

    делает правки

--- source/js/main.js
+++ source/js/main.js
@@ -1,42 +1,42 @@
 'use strict';
 
 (function () {
-  if (document.getElementsByClassName('page-body')) {
-    var body = document.querySelector('.page-body');
-  }
+  var body = document.querySelector('.page-body');
   var header = document.querySelector('.navigation');
   var navButton = document.querySelector('.navigation__button-menu');
   var menu = document.querySelector('.navigation__list');
   var navLink = document.querySelectorAll('.navigation__item');
 
-  header.classList.remove('navigation--nojs');
-  menu.classList.remove('navigation__list--nojs');
+  if (header && navButton && menu && navLink && body) {
+    header.classList.remove('navigation--nojs');
+    menu.classList.remove('navigation__list--nojs');
 
-  var closeMenu = function () {
-    header.classList.remove('navigation--opened');
-    header.classList.add('navigation--close');
-    menu.classList.add('navigation__list--close');
-    body.classList.remove('page-body--open-menu');
-  };
+    var closeMenu = function () {
+      header.classList.remove('navigation--opened');
+      header.classList.add('navigation--close');
+      menu.classList.add('navigation__list--close');
+      body.classList.remove('page-body--open-menu');
+    };
 
-  var openMenu = function () {
-    header.classList.remove('navigation--close');
-    menu.classList.remove('navigation__list--close');
-    header.classList.add('navigation--opened');
-    body.classList.add('page-body--open-menu');
-  };
+    var openMenu = function () {
+      header.classList.remove('navigation--close');
+      menu.classList.remove('navigation__list--close');
+      header.classList.add('navigation--opened');
+      body.classList.add('page-body--open-menu');
+    };
 
-  navButton.addEventListener('click', function () {
-    if (header.classList.contains('navigation--close')) {
-      openMenu();
-    } else {
-      closeMenu();
-    }
-  });
-
-  for (var i = 0; i < navLink.length; i++) {
-    navLink[i].addEventListener('click', function () {
-      closeMenu();
+    navButton.addEventListener('click', function () {
+      if (header.classList.contains('navigation--close')) {
+        openMenu();
+      } else {
+        closeMenu();
+      }
     });
+
+    for (var i = 0; i < navLink.length; i++) {
+      navLink[i].addEventListener('click', function () {
+        closeMenu();
+      });
+    }
   }
 })();
